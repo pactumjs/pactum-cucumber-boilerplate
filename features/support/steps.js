99242fc18c05b3e322c6e45deb0fe59d6433ea95
HimChin <asa.anudeep4@gmail.com>
2020-09-20 16:20:58 +0530

    added more steps

--- features/support/steps.js
+++ features/support/steps.js
@@ -18,3 +18,19 @@ When('I receive a response', async function () {
 Then('response should have a status {int}', function (code) {
   spec.response().to.have.status(200);
 });
+
+Then(/^response should have a json$/, function (json) {
+  spec.response().to.have.json(JSON.parse(json));
+});
+
+Then(/^response should have a json like$/, function (json) {
+  spec.response().to.have.jsonLike(JSON.parse(json));
+});
+
+Then(/^response should have a json schema$/, function (json) {
+  spec.response().to.have.jsonSchema(JSON.parse(json));
+});
+
+Then(/^response should have a body$/, function (body) {
+  spec.response().to.have.body(body);
+});
